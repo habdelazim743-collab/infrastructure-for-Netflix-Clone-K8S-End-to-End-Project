f83af2e295c4636cda646378567a6daf96fe8e6e
habdelazim743-collab <habdelazim743@gmail.com>
2026-02-01 17:06:27 +0200

    edits in k8s addons

--- provider.tf
+++ provider.tf
@@ -27,7 +27,7 @@ provider "kubernetes" {
 # ==========================
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
+    host = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(
       data.aws_eks_cluster.this.certificate_authority[0].data
     )
